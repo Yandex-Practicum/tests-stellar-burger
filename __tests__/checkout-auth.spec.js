5d8cda6273ca43d6e1a7ccaaf1da6fc865a6c67d
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-02-22 13:54:37 +0300

    Update checkout-auth.spec.js

--- __tests__/checkout-auth.spec.js
+++ __tests__/checkout-auth.spec.js
@@ -74,13 +74,7 @@ test.describe('Страница оформления заказа авториз
     await page.locator(`a[href="/ingredients/${sauceItem._id}"] + button`).click();
     await page.locator(`a[href="/ingredients/${mainItem._id}"] + button`).click();
     await page.getByRole('button', { name: 'Оформить заказ' }).click();
-    let response;
-    try {
-      response = await responsePromise;
-    } catch (error) {
-      await page.getByRole('button', { name: 'Оформить заказ' }).click();
-      response = await responsePromise;
-    }
+    const response = await responsePromise;
     const res = await response.json();
     await page.waitForSelector('#modals img');
     await expect(page.locator('#modals')).toContainText(String(res.order.number));
